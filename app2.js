cc87952104a743f9fb6e2edacb3e509767279ef2
DamBinh1 <118426540+DamBinh1@users.noreply.github.com>
2023-02-02 16:36:05 +0700

    Update app2.js

--- app2.js
+++ app2.js
@@ -1,8 +1,8 @@
-var validationApp = angular.module("validationApp",[]);
-validationApp.controller("mainController",function($scope){
+var validationApp = angular.module('validationApp',[]);
+validationApp.controller('mainController',function($scope){
     $scope.submitForm = function(isValid){
         if(isValid){
-            alert("form is Valid");
+            alert('form is Valid');
         }
     };
 });
